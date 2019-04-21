7e0daf6f11839381bffddcfa63be2480b943205a
Trystan Auddino <trystan8888@gmail.com>
2019-04-21 15:27:00 +0000

    Libraries added, testing if commit is  successful.

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
-#include <std.h>
+/*
+ * Assignment 1: Ceaser Cipher 
+ * by Trystan Auddino c3329902
+ */
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+//main function with some ideas for integers 
 int main() {
+    
+    int key = 1;
+    
+    int a = 0;
+    int b = 1;
+    
     return 0;
 }
