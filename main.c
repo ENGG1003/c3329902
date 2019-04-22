8b3065a735ad6915db5cb5041c676d648f81ef47
Trystan Auddino <trystan8888@gmail.com>
2019-04-22 06:41:06 +0000

    New code to account for whitespace, added options for user to select encrypt/decrypt

--- main.c
+++ main.c
@@ -13,18 +13,39 @@ void inputCipher(char* text, int key); // Defining the function for taking user 
 int main(void) { //Main function 
     
     int key = 1; 
+    int d;
     char text[500]; //Maximum characters set to 500, should be enough
     
+    int increment = 0;
+    int newValue;
+    char value;
+    
     printf("Input text: "); //User inputs text
-   
     fgets(text, sizeof(text), stdin); //Asks user for text, stores in "text" variable
+    printf("%c", text);
     
-    printf("Encrypted text: ");
+    printf("\nPlease enter key: \t");
+    scanf("%d", &key);
     
-    inputCipher(text, key); //Insets result after printf statement
+    printf("\nPlease choose following options:\n");
+    printf("1 = Encrypt the text.\n");
+    printf("2 = Decrypt the text.\n");
+    scanf("%d", &d);
+   
+   
+   switch(d)
+   {
+   case 1:
+        for(increment = 0; (increment < 100 && text[increment] != '\0'); increment++)
+            text[increment] = text[increment] + key; //Formula adds key to ASCII value
+            printf("\nEncrypted string: %s\n", text);
+            break;
+   // printf("Encrypted text: ");
+    
+   // inputCipher(text, key); //Inserts result after printf statement
 }
 
-void inputCipher(char* text, int key) {
+/*void inputCipher(char* text, int key) {
     
     int increment = 0;
     int newValue;
@@ -35,6 +56,6 @@ void inputCipher(char* text, int key) {
         newValue = (char)(value); 
         printf("%c", value); // Print our encryted text
         increment++; //Increments the input variable 
-    }
+    }*/
     
-}
+   }
