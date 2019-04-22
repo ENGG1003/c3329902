0672c81b994882ec21621d3ea28051b2d2ac9666
Trystan Auddino <trystan8888@gmail.com>
2019-04-22 05:50:01 +0000

    Saving changes, Encrypt almost done, just need to add decrypt and switch case

--- main.c
+++ main.c
@@ -10,29 +10,31 @@
 
 void inputCipher(char* text, int key); // Defining the function for taking user input, taking in text and key value
 
-int main(void) { //main function 
+int main(void) { //Main function 
     
     int key = 1; 
     char text[500]; //Maximum characters set to 500, should be enough
     
     printf("Input text: "); //User inputs text
    
-    fgets(text, sizeof(text), stdin); //Asks user for text, stores in "text" veriable
+    fgets(text, sizeof(text), stdin); //Asks user for text, stores in "text" variable
     
-    inputCipher(char* text, int key)
+    printf("Encrypted text: ");
+    
+    inputCipher(text, key); //Insets result after printf statement
 }
 
 void inputCipher(char* text, int key) {
     
-    int input = 0;
-    char newValue;
+    int increment = 0;
+    int newValue;
     char value;
     
-    while(text[input] != '\0' && strlen(text)-1 > input){
-        value = ((int)text[input] -97 + key) % 26 + 97;  //returns the character as a number, ( The -97 is counteract the ASCII values and start from 0)
-        newValue = (char)(value);
-        
-        printf("\n %c", value);
+    while(text[increment] != '\0' && strlen(text)-1 > increment){ //While variable "text" is at start, does not equal null and string length of "text" -1 is > "input" variable
+        value = ((int)text[increment] -97 + key) % 26 + 97;  //Returns the character as a number, ( The -97 is counteract the ASCII values and start from 0)
+        newValue = (char)(value); 
+        printf("%c", value); // Print our encryted text
+        increment++; //Increments the input variable 
     }
-    return 0;
+    
 }
