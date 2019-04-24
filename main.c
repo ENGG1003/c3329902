87d71851dee3959a1771fd6e52d56e6c846f921f
Trystan Auddino <trystan8888@gmail.com>
2019-04-24 04:23:20 +0000

    Fixed whitespace code, everything works as it should now

--- main.c
+++ main.c
@@ -7,55 +7,87 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-void inputCipher(char* text, int key); // Defining the function for taking user input, taking in text and key value
-
-int main(void) { //Main function 
+int main() { //Main function 
     
-    int key = 1; 
-    int d;
-    char text[500]; //Maximum characters set to 500, should be enough
+  
+    char text[500], ch; //Maximum characters set to 500, should be enough
+    int key;
+    int d=1;
+    int increment;
+ 
     
-    int increment = 0;
-    int newValue;
-    char value;
-    
-    printf("Input text: "); //User inputs text
-    fgets(text, sizeof(text), stdin); //Asks user for text, stores in "text" variable
-    printf("%c", text);
-    
-    printf("\nPlease enter key: \t");
+  	printf("Enter a message to encrypt/decrypt: "); //User inputs text
+    gets(text); //Asks user for text, stores in "text" variable
+      
+    printf("Please enter key: ");
     scanf("%d", &key);
     
-    printf("\nPlease choose following options:\n");
-    printf("1 = Encrypt the text.\n");
-    printf("2 = Decrypt the text.\n");
+    printf("Please choose either encryption or decryption (1: Encrypt, 2: Decrypt)\n");
     scanf("%d", &d);
    
    
-   switch(d)
-   {
+   switch(d) {
    case 1:
-        for(increment = 0; (increment < 100 && text[increment] != '\0'); increment++)
-            text[increment] = text[increment] + key; //Formula adds key to ASCII value
-            printf("\nEncrypted string: %s\n", text);
+       for(increment = 0; text[increment] != '\0'; ++increment){
+		ch = text[increment];
+		
+		if(ch >= 'a' && ch <= 'z'){
+			ch = ch + key;
+			
+			if(ch > 'z'){
+				ch = ch - 'z' + 'a' - 1;
+			}
+			
+			text[increment] = ch;
+		}
+		else if(ch >= 'A' && ch <= 'Z'){
+			ch = ch + key;
+			
+			if(ch > 'Z'){
+				ch = ch - 'Z' + 'A' - 1;
+			}
+			
+			text[increment] = ch;
+		}
+		
+	}
+		printf("Encrypted message: %s", text);
             break;
-   // printf("Encrypted text: ");
-    
-   // inputCipher(text, key); //Inserts result after printf statement
-}
+            
+    case 2:
+	    
+	for(increment = 0; text[increment] != '\0'; ++increment){
+		ch = text[increment];
+		
+		if(ch >= 'a' && ch <= 'z'){
+			ch = ch - key;
+			
+			if(ch < 'a'){
+				ch = ch + 'z' - 'a' + 1;
+			}
+			
+			text[increment] = ch;
+		}
+		else if(ch >= 'A' && ch <= 'Z'){
+			ch = ch - key;
+			
+			if(ch < 'A'){
+				ch = ch + 'Z' - 'A' + 1;
+			}
+			
+			text[increment] = ch;
+		}
+	}
+	
+	printf("Decrypted message: %s", text);
+	break;
+	}
+	
+	
 
-/*void inputCipher(char* text, int key) {
+	
+	return 0;
+}
     
-    int increment = 0;
-    int newValue;
-    char value;
-    
-    while(text[increment] != '\0' && strlen(text)-1 > increment){ //While variable "text" is at start, does not equal null and string length of "text" -1 is > "input" variable
-        value = ((int)text[increment] -97 + key) % 26 + 97;  //Returns the character as a number, ( The -97 is counteract the ASCII values and start from 0)
-        newValue = (char)(value); 
-        printf("%c", value); // Print our encryted text
-        increment++; //Increments the input variable 
-    }*/
-    
-   }
+   
+
