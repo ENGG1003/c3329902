4aa564946889d7935921f12ebe25a73a6410482e
Trystan Auddino <trystan8888@gmail.com>
2019-04-25 06:14:48 +0000

    Started creating a better menu with single switch case, ready for substitution method to be added

--- main.c
+++ main.c
@@ -6,28 +6,35 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define MAX 100 
 
 int main() { //Main function 
     
   
     char text[500], ch; //Maximum characters set to 500, should be enough
-    int key;
-    int d=1;
-    int increment;
+    int key; //This key determines how many letters the rotation cipher will move
+    int d=0; //For switch statement
+    int increment; //Used in for loop as counter
  
     
   	printf("Enter a message to encrypt/decrypt: "); //User inputs text
-    gets(text); //Asks user for text, stores in "text" variable
-      
-    printf("Please enter key: ");
-    scanf("%d", &key);
+    fgets(text, MAX, stdin); //Asks user for text, stores in "text" variable
     
-    printf("Please choose either encryption or decryption (1: Encrypt, 2: Decrypt)\n");
-    scanf("%d", &d);
+    printf("Please choose from menu:\n"); //Gives user choice of next for loop
+    printf("1: Ceaser encrypt\n");
+    printf("2: Ceaser decrypt\n");
+    printf("3: Substitution encrypt\n");
+    printf("4: Substitution decrypt\n");
+    
+    scanf("%d", &d); //This scans for user input, then sends either 1 or 2 to variable d which is used to pick a statement in the (switch statement) flow control
    
    
    switch(d) {
    case 1:
+   
+    printf("Please enter key: "); 
+    scanf("%d", &key); //Standard input, scans for key off user
+    
        for(increment = 0; text[increment] != '\0'; ++increment){
 		ch = text[increment];
 		
@@ -55,6 +62,9 @@ int main() { //Main function
             break;
             
     case 2:
+    
+    printf("Please enter key: "); 
+    scanf("%d", &key); //Standard input, scans for key off user
 	    
 	for(increment = 0; text[increment] != '\0'; ++increment){
 		ch = text[increment];
@@ -81,6 +91,17 @@ int main() { //Main function
 	
 	printf("Decrypted message: %s", text);
 	break;
+	
+	case 3:
+	
+	printf("well well well");
+	break;
+	
+	case 4: 
+	
+	printf("hi");
+	break;
+	
 	}
 	
 	
